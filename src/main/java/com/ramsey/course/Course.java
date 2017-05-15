f841819705edb0c26cbe266b9a460882a3012ce3
joelramsey <joel.d.ramsey@gmail.com>
2017-05-14 22:06:31 -0600

    Added a data loader for seeding

--- src/main/java/com/ramsey/course/Course.java
+++ src/main/java/com/ramsey/course/Course.java
@@ -1,22 +1,35 @@
 package com.ramsey.course;
 
 import com.ramsey.core.BaseEntity;
+import com.ramsey.review.Review;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Course extends BaseEntity {
     private String title;
     private String url;
+    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL)
+    private List<Review> reviews;
 
     protected Course() {
         super();
+        reviews = new ArrayList<>();
     }
 
     public Course(String title, String url) {
         this();
         this.title = title;
         this.url = url;
+    }
+
+    public void addReview(Review review) {
+        review.setCourse(this);
+        reviews.add(review);
     }
 
     public String getTitle() {
--- src/main/java/com/ramsey/review/Review.java
+++ src/main/java/com/ramsey/review/Review.java
@@ -1,17 +1,34 @@
 package com.ramsey.review;
 
 import com.ramsey.core.BaseEntity;
+import com.ramsey.course.Course;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Review extends BaseEntity {
+    private int rating;
+    private String description;
+    @ManyToOne
+    private Course course;
 
     protected Review() {
         super();
     }
-    private int rating;
-    private String description;
+
+    public Review(int rating, String description) {
+        this.rating = rating;
+        this.description = description;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
 
     public int getRating() {
         return rating;

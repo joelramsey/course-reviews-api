273a9c32c18fea2f90ef211cd8ba4f359a6a9a37
joelramsey <joel.d.ramsey@gmail.com>
2017-05-15 20:11:11 -0600

    added api versioning

--- src/main/java/com/ramsey/course/Course.java
+++ src/main/java/com/ramsey/course/Course.java
@@ -6,6 +6,7 @@ import com.ramsey.review.Review;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
